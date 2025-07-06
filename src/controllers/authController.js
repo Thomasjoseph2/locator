55080173b8f84eb8f3ea5b1bd7ca89fd6863b2ed
Thomas joseph <tj089502@gmail.com>
2025-07-06 13:51:34 +0530

    vercel deployment

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
 
 const generateTokens = (user) => {
     const accessToken = jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '60m' });
+    
     const refreshToken = jwt.sign({ id: user._id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
     return { accessToken, refreshToken };
 };
